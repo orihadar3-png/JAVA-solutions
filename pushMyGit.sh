e16ed2854b49a45473b07c7becf22693c35f3f1a
ori <orihadar3@gmail.com>
2025-10-23 13:42:16 +0300

    AutoUpdate

--- pushMyGit.sh
+++ pushMyGit.sh
@@ -1,4 +1,8 @@
-#!/bin/bash 
-date   > fileDatepush.txt
-git add . && git commit -m  "Update center" && git push
-
+#!/bin/bash
+d=$(date +"%Y-%m-%d_%H:%M:%S")
+echo "$d" > fileDatepush.txt
+git add .
+git commit -m "AutoUpdate"
+b=$(git rev-parse --abbrev-ref HEAD)
+git pull --rebase origin "$b"
+git push origin "$b"
