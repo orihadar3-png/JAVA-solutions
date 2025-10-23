6f70862438502c85357d66be5ca89dd4bd4625fb
ori <orihadar3@gmail.com>
2025-10-23 14:05:56 +0300

    AutoUpdate

--- pushMyGit.sh
+++ pushMyGit.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# קביעת תאריך ושעה
 d=$(date +"%Y-%m-%d_%H:%M:%S")
 echo "$d" > fileDatepush.txt
-git add .
-git commit -m "AutoUpdate"
-b="temp-branch"
-git pull --rebase origin "$b"
-git push origin "$b"
 
+# מעבר לענף temp-branch (יוצר אותו אם לא קיים)
+git checkout -B temp-branch
+
+# הוספת קבצים ל-commit
+git add .
+
+# ביצוע commit אוטומטי
+git commit -m "AutoUpdate"
+
+# משיכה מהענף לפני push
+git pull --rebase origin temp-branch
+
+# שליחת השינויים לענף
+git push origin temp-branch
