ae339388d61ab06d5b30c277451056d6c83e752f
ori <orihadar3@gmail.com>
2025-11-03 13:50:40 +0200

    AutoUpdate

--- example.java
+++ example.java
@@ -5,3 +5,25 @@ public class Main {
     }
 
 
+
+// דוגמה   לקלט     של  משתנים    
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("הכנס את המספר הראשון: ");
+        // int num1 = scanner.nextInt();
+        double num1=scanner.nextDouble();
+
+        System.out.print("הכנס את המספר השני: ");
+        //int num2 = scanner.nextInt();
+        double num2=scanner.nextDouble();
+
+        System.out.println("המספרים שהוזנו הם: " + num1 + " ו-" + num2);
+
+    }
+}
+*/
