25a2705883d2b8aec34ae3b534d2f1d1f86025f2
ori <orihadar3@gmail.com>
2025-11-30 07:51:24 +0200

    Upload all project files

--- example.java
+++ example.java
@@ -32,3 +32,18 @@ public class Main {
     }
 }
 /////////////////////////////////////////////////////////////
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("הכנס תו אחד: ");
+        char c = sc.next().charAt(0); // לוקח את התו הראשון מהמחרוזת שהוזנה
+
+        System.out.println("התו שהוזן הוא: " + c);
+    }
+}
+
